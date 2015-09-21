4358cbc225989c699256e2baff5de2428c00297d
Charityzed <czablon@cytonn.com>
2015-09-21 19:41:38 +0300

    angular1

---
+++ js/app.js
@@ -0,0 +1,77 @@
+(function (angular){
+	'use strict';
+	var myApp=angular.module('menu', []);
+
+	myApp.controller('MenuContaroller',['$scope', function($scope){
+
+	$scope.Main='DOWNLOADS';
+	$scope.About="TOPICALS";
+	$scope.Gallery="RESEARCH"
+	
+
+	
+	$scope.English=function(){
+	$scope.Main="DOWNLOADS";
+	$scope.About="TOPICALS";
+	$scope.Gallery="RESEARCH"
+	};
+
+	$scope.Swahili=function(){
+	$scope.Main="TOA KWA MTANDAO";
+	$scope.About="MADA";
+	$scope.Gallery="UTAFITI"
+};
+
+	$scope.French=function(){
+	$scope.Main="TELECHANGER";
+	$scope.About="TOPIQUES";
+	$scope.Gallery="RECHERCHE"
+	};
+	}]);
+	myApp.controller('ListController',function($scope) {
+	$scope.reports=[
+	{ 	name:'Cyton Weekly Report #20',
+		Author:'Cytonn research Team',
+		date:"20/June/2015"
+
+
+	},
+	{ 	name:'Cyton Weekly Report #24',
+		Author:'Cytonn research Team',
+		date:"20/July/2015"
+
+
+	},
+	{ 	name:'Cyton Weekly Report #27',
+		Author:'Cytonn research Team',
+		date:"20/August/2015"
+
+
+	},
+	{ 	name:'Cyton Weekly Report #31',
+		Author:'Cytonn research Team',
+		date:"20/Sepembet/2015"
+
+
+	},
+	{ 	name:'Cyton Monthly Report #35',
+		Author:'Cytonn research Team',
+		date:"12/September/2015"
+
+
+	},
+	{ 	name:'Cyton First Half Report 2015 #36',
+		Author:'Cytonn research Team',
+		date:"21//2015"
+
+
+	}
+	];
+	
+});
+
+
+
+	
+
+})(window.angular)
---
+++ js/app1.js
@@ -0,0 +1,48 @@
+
+var myApp2=angular.module('list1',[]);
+myApp2.controller('ListController',function($scope) {
+	$scope.reports=[
+	{ 	name:'Cyton Weekly Report #20',
+		Author:'Cytonn research Team',
+		date:"20/June/2015"
+
+
+	},
+	{ 	name:'Cyton Weekly Report #24',
+		Author:'Cytonn research Team',
+		date:"20/July/2015"
+
+
+	},
+	{ 	name:'Cyton Weekly Report #27',
+		Author:'Cytonn research Team',
+		date:"20/August/2015"
+
+
+	},
+	{ 	name:'Cyton Weekly Report #31',
+		Author:'Cytonn research Team',
+		date:"20/Sepembet/2015"
+
+
+	},
+	{ 	name:'Cyton Monthly Report #35',
+		Author:'Cytonn research Team',
+		date:"12/September/2015"
+
+
+	},
+	{ 	name:'Cyton First Half Report 2015 #36',
+		Author:'Cytonn research Team',
+		date:"21//2015"
+
+
+	}
+	];
+	
+});
+
+	
+
+
+// })(window.angular)
